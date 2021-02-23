1d48d959050ff54bde02bd486afdb59fe725ed94
Adams Paul <adamsohiani@gmail.com>
2021-02-23 23:50:07 +0100

    Final Fixes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -25,7 +25,7 @@ class ApiController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed']
+            'password' => ['required', 'string', 'min:8']
         ]);
         if ($validator->fails()) {
             return response()->json([
